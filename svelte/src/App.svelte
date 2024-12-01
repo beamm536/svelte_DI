882b56fc5c3bcab6de6c2247dc7a5cd3c284b9e5
beamm536 <beamm023@gmail.com>
2024-12-01 20:09:23 +0100

    pagina de login con verificacion
    faltan mas estiloss

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,10 +1,30 @@
 <script>
 //aqui las importaciones
+import Counter from './lib/Counter.svelte';
+import InputUsuario from './lib/InputUsuario.svelte';
 </script>
 
 <main>
+  <link rel="stylesheet" href="css/styles.css">
   <div class="viewContainer">
+    <!-- <Counter /> -->
 
+      <h1>Welcome Back!</h1>
+      <p>Login</p>
+
+      <div class="form-container">
+        
+        <InputUsuario />
+
+        
+        <p>Or signup with Google</p>
+        <button class="secondary">Continue with Google</button>
+
+        <p>Don't have an account? <a href="#">Signup</a></p>
+      </div>
+
+      <br>
+      <p>Pruebas para el email: usuario.ejemplo@dominio.com</p>
   </div>
 </main>
 
---
+++ svelte/src/lib/InputUsuario.svelte
@@ -0,0 +1,36 @@
+<script>
+//PAGINA DE LOGIN
+    let email = "";
+    let valido = false;
+
+    let password = "";
+    
+
+    function submit() {
+        console.log("Email:", email);
+        console.log("Password:", password); 
+    }
+
+    function validacionEmail() {
+        const regex = /^[\w-.]+@[\w-]+\.[a-z]{2,7}$/i;
+        if (regex.test(email)) {
+            valido = true;
+        } else {
+            valido = false;
+        }
+    }
+</script>
+
+<div class="container">
+
+<link rel="stylesheet" href="css/stylesLogin.css">
+
+
+<div class="email-contenedor">
+    <input type="email" bind:value={email} on:input={validacionEmail} placeholder="Email">
+    <span class="estadoEmail">{valido ? 'Email ✅' : 'Email ❌'}</span>
+</div>
+    <input type="password" bind:value={password} placeholder="Password"><br>
+
+    <button class="primary" on:click={submit}>Login</button>
+</div>
